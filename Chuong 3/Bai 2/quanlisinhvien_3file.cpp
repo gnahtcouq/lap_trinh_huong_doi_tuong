ba97056e953cac77138ea1a39d3a68b3a3ea0a04
Quốc Thắng <comehere.thang@gmail.com>
2021-12-26 22:01:17 +0700

    26/12/21
    26/12/21

---
+++ Chuong 3/Bai 2/quanlisinhvien_3file.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Ngay.h
+class Ngay {
+private:
+  int ngay, thang, nam;
+public:
+  void nhap();
+  void xuat();
+};
+
+
+// SinhVien.h
+class SinhVien {
+private:
+  string ten;
+  float diemToan, diemVan;
+  Ngay ngaySinh;
+public:
+  void nhap();
+  void xuat();
+  float tinhDiemTrungBinh();
+};
+
+
+int main() {
+  SinhVien sv1;
+  sv1.nhap();
+  sv1.xuat();
+  float dtb = sv1.tinhDiemTrungBinh();
+  cout << "\nDiem trung binh = " << dtb;
+
+  system("pause");
+  return 0;
+}
+
+// SinhVien.cpp
+void SinhVien::nhap() {
+  fflush(stdin);
+  cout << "Nhap ten: ";
+  getline(cin, ten);
+  cout << "Nhap diem toan: ";
+  cin >> diemToan;
+  cout << "Nhap diem van: ";
+  cin >> diemVan;
+  cout << "Ngay sinh: ";
+  ngaySinh.nhap();
+}
+
+void SinhVien::xuat() {
+  cout << "\nTen: " << ten;
+  cout << "\nDiem toan: " << diemToan;
+  cout << "\nDiem van: " << diemVan;
+  cout << "\nSinh ngay: ";
+  ngaySinh.xuat();
+}
+
+float SinhVien::tinhDiemTrungBinh() {
+  return (diemToan + diemVan) / 2;
+}
+
+
+// Ngay.cpp
+void Ngay::nhap() {
+  cout << "\n\tNhap ngay: ";
+  cin >> ngay;
+  cout << "\tNhap thang: ";
+  cin >> thang;
+  cout << "\tNhap nam: ";
+  cin >> nam;
+}
+
+void Ngay::xuat() {
+  cout << "Ngay " << ngay << " thang " << thang << " nam " << nam;
+}
