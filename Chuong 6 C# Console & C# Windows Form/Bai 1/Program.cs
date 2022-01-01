cb9c6a310c7729c0caa86a20996ee467c35eea5d
Quốc Thắng <comehere.thang@gmail.com>
2022-01-01 18:59:22 +0700

    1/1/22
    1/1/22

--- Chuong 6 C# Console & C# Windows Form/Bai 1/Program.cs
+++ Chuong 6 C# Console & C# Windows Form/Bai 1/Program.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿using System.Collections;
+using System;
 
 namespace Bai_1 {
   class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      // System.Console.Write("Hello world");
+      // System.Console.ReadKey();
+      // Console.WriteLine("Hello World!");
+
+
+      // int x = -5;
+      // Math.Abs(x);
+
+      // ArrayList arr = new ArrayList();
+      // arr.Add("thang");
+      // arr.Add("dep");
+      // arr.Add("trai");
+      // arr.Add(29);
+      // arr.Add(10);
+
+      // arr[0] += "ahihi";
+      // // arr[3] += "ahihi";
+
+      // int size = arr.Count;
+
+      // for (int i = 0; i < size; i++) {
+      //   Console.Write("{0}\n", arr[i]);
+      // }
+
+      // Console.ReadKey();
     }
   }
 }
