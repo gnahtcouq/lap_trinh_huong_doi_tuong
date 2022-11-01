982609abf7209f52a5f90199eefb360879008af8
Quốc Thắng <comehere.thang@gmail.com>
2022-11-01 15:40:44 +0700

    /
    /

---
+++ Chuong 3 Lam Quen OOP (2022)/Bai 3 Giai Bai Tap OOP/try catch/try catch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch {
+    internal class Class1 {
+        static void Main(string[] args) {
+            int x;
+        thangdeptrai:
+            try {
+                Console.Write("\nNhap x = ");
+                x = int.Parse(Console.ReadLine());
+            } catch {
+                Console.Write("\nKieu du lieu khong hop le. Xin nhap lai");
+                goto thangdeptrai;
+            }
+            Console.Write("\nx = {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
